8070c48c9d217cb10e7a5df731973b04dd1bc9f7
Your Name <you@example.com>
2017-09-27 10:57:54 +0800

    vnshop

---
+++ client/src/view/GoodsList.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+      <HeadNav/>
+        <NavBread><span>商品</span></NavBread>
+    <div class="accessory-result-page accessory-page">
+        <div class="container">
+            <div class="filter-nav">
+                <span class="sortby">Sort by:</span>
+                <a href="javascript:void(0)" class="default cur">Default</a>
+                <a href="javascript:void(0)" class="price" @click="sortGoods">价格 <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+                <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+            </div>
+            <div class="accessory-result">
+                <!-- filter -->
+                <div class="filter stopPop" id="filter">
+                    <dl class="filter-price">
+                        <dt>Price:</dt>
+                        <dd><a href="javascript:void(0)">All</a></dd>
+                        <dd>
+                            <a href="javascript:void(0)">0 - 100</a>
+                        </dd>
+                        <dd>
+                            <a href="javascript:void(0)">100 - 500</a>
+                        </dd>
+                        <dd>
+                            <a href="javascript:void(0)">500 - 1000</a>
+                        </dd>
+                        <dd>
+                            <a href="javascript:void(0)">1000 - 2000</a>
+                        </dd>
+                    </dl>
+                </div>
+
+                <!-- search result accessories list -->
+                <div class="accessory-list-wrap">
+                    <div class="accessory-list col-4">
+                        <ul>
+                            <li v-for="(item,index) in list" :key='index'>
+                                <div class="pic">
+                                    <a href="#"><img v-lazy="'/static/img/' + item.productImage" alt=""></a>
+                                </div>
+                                <div class="main">
+                                    <div class="name">{{item.productName}}</div>
+                                    <div class="price">{{item.salePrice}}</div>
+                                    <div class="btn-area">
+                                        <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  <Footer/>
+  </div>
+</template>
+<script>
+import HeadNav from '@/components/Head'
+import NavBread from '@/components/NavBread'
+import Footer from '@/components/Footer'
+import axios from 'axios'
+export default {
+    data(){
+        return{
+            list:[],
+        }
+    },
+    components:{
+        HeadNav,
+        NavBread,
+        Footer
+    },
+    created(){
+        this.getGoods();
+    },
+    methods:{
+        getGoodsList(){
+            axios.get('测试接口')
+            .then(res=>{
+                res = res.data.data;
+                // this.list = res;
+            })
+        },
+        getGoods(){let sort = this.sortFlag ? 1 : -1;
+          axios.get('/goods/list',{params:{sort:sort}}).then(res=>{
+            this.list= res.data.result;
+            console.log(res.data.result);
+          })
+        },
+      sortGoods(){
+        this.sortFlag = !this.sortFlag;
+        this.getGoods();
+      }
+    }
+}
+</script>
+<style>
+
+</style>
+
+
---
+++ server/routes/goods.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Goods = require('../models/goods')
+mongoose.connect('mongodb://localhost:27017/sever',{useMongoClient: true});
+
+mongoose.connection.on('connected', function() {
+    console.log("mongondb connected success");
+})
+
+mongoose.connection.on('error', function() {
+    console.log("mongondb connected fail");
+})
+
+mongoose.connection.on('disconnected', function() {
+    console.log("mongondb connected disconnected");
+})
+
+router.get("/list", function(req, res, next) {
+    let sort = req.param('sort');
+    let goodModel = Goods.find({}).sort({ 'salePrice': sort });
+    goodModel.exec({}, function(err, doc) {
+        res.json({ status: 0, result: doc })
+    })
+});
+
+module.exports = router;
